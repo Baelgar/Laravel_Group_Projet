ae2e014d3c10d548123b5103e27fac938dc9d61f
haikizoku <gnorvene@gmail.com>
2018-06-19 10:14:01 +0200

    add model and  relashioship

--- app/Module.php
+++ app/Module.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    public function Salle()
+    {
+        return $this->hasMany('App\Salle');
+    }
 }
+
--- app/Salle.php
+++ app/Salle.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salle extends Model
 {
-    //
+    public function user()
+    {
+        return $this->hasMany('App\User');
+    }
+
+    public function Reservation()
+    {
+        return $this->hasMany('App\Reservation');
+    }
+
+    public function Forfait()
+    {
+        return $this->hasMany('App\Reservation');
+    }
 }
