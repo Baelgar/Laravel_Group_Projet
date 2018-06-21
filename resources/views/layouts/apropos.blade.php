24c55b18ec7a24a1f96656307298488a75344278
admin <admin@iMac3-70.local>
2018-06-21 12:09:50 +0200

    add Apropos page

---
+++ resources/views/layouts/apropos.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.base')
+
+@section('title','Descriptif salle')
+@section('content')
+    <div class="container">
+        <div class="jumbotron jumbotron-fluid text-center">
+            <h1 >A propos de Loc'all</h1>
+            <p>Site de location de salles</p>
+            <h3>Adresse</h3>
+            <p>Version 0.0.1</p>
+        </div>
+    </div>
+@endsection
