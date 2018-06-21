963927c4418bf38db19c66403fe159feb05671cf
gregory <gnorvene@gmail.com>
2018-06-21 02:44:57 +0200

    add  fake  user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
             $user = factory(App\User::class)->create(['email' => 'admin@local.fr','is_admin' => '1']);
+            $user2 = factory(App\User::class)->create(['name' => 'user', 'email' => 'user@local.fr','is_admin' => '0']);
     }
 }
