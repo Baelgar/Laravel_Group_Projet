42c47c13968685d6074a0595b5b52c1abad14303
gregory <gnorvene@gmail.com>
2018-06-21 03:02:08 +0200

    add  fake  module

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,9 @@ class DatabaseSeeder extends Seeder
     {
             $user = factory(App\User::class)->create(['email' => 'admin@local.fr','is_admin' => '1']);
             $user2 = factory(App\User::class)->create(['name' => 'user', 'email' => 'user@local.fr','is_admin' => '0']);
+            $module = factory(App\Module::class)->create(['name' => 'animation Adulte']);
+            $module = factory(App\Module::class)->create(['name' => 'animation enfant']);
+            $module = factory(App\Module::class)->create(['name' => 'dj']);
+            $module = factory(App\Module::class)->create(['name' => 'pianiste']);
     }
 }
